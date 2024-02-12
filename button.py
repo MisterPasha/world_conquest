cdda749ac7a492eecbb3074492c920df7b5bd3a7
MisterPasha <pavel.jermolajev99@gmail.com>
2024-02-12 15:13:55 +0000

    changed everything

--- button.py
+++ button.py
@@ -1,51 +1,37 @@
 import pygame
+
 pygame.init()
 
-hover_sound = pygame.mixer.Sound("C:\\Sussex\\Programming\\Software_Eng\\sounds\\button_click.wav")
-
-
-def drop_colors(button_list, old_color, new_color):
-    for button_ in button_list:
-        if button_.color == old_color:
-            button_.change_color(new_color)
 
 class Button:
-    BLACK = pygame.Color("black")
-
-    def __init__(self, rect, color, hover_color, text, font_size, action, font=None):
-        self.color = color
-        self.hover_color = hover_color
-        self.rect = rect
+    def __init__(self, image, pos, text, font_size, width, height, font=None, action=None):
+        self.image = image
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.x, self.y = pos
+        self.position = pos
+        self.rect = self.image.get_rect(topleft=pos)
         self.text = text
+        self.font_size = font_size
         self.action = action
-        self.hover = False
-        self.was_hovered = False
-        self.font_button = pygame.font.Font(font, font_size)
+        self.font = pygame.font.Font(font, self.font_size)
 
-    # Function to draw a button
-    def create_button(self, surface, width=0, br=-1, text_color=BLACK):
-        color = self.hover_color if self.hover else self.color
-        pygame.draw.rect(surface, color, self.rect, width=width, border_radius=br)
-        text_surface = self.font_button.render(self.text, True, text_color)
-        text_rect = text_surface.get_rect(center=self.rect.center)
-        surface.blit(text_surface, text_rect)
+    def draw(self, screen):
+        # Draw the button image
+        screen.blit(self.image, self.position)
 
-    def check_hover(self, mouse_pos):
-        if self.rect.collidepoint(mouse_pos):
-            self.hover = True
-            if not self.was_hovered:  # If it's the first time hovering
-                hover_sound.play()  # Play sound
-                self.was_hovered = True  # Update the flag so sound is not played again
-        else:
-            self.hover = False
-            self.was_hovered = False  # Reset the flag when not hovering
+        # Render the text
+        text_surf = self.font.render(self.text, True, (0, 0, 0))  # Black text
+        # Center the position of the text
+        text_rect = text_surf.get_rect(center=self.rect.center)
+        # Draw the text
+        screen.blit(text_surf, text_rect)
 
-    def check_click(self, mouse_pos):
-        if self.hover:
-            self.action()
+    def check_click(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                if self.action:
+                    self.action()
 
-    # changes a color of the button
-    def change_color(self, new_color):
-        self.color = new_color
-
-
+    def change_image(self, new_image):
+        new_image = pygame.transform.scale(new_image, (self.image.get_width(), self.image.get_height()))
+        self.image = new_image
---
+++ main_menu.py
@@ -0,0 +1,128 @@
+import pygame
+from button import Button
+import numpy as np
+
+pygame.init()
+
+
+class MainMenu:
+    background_image = pygame.image.load("images\\background_main_high.png")
+    button_image = pygame.image.load("images\\button_high.png")
+    logo_image = pygame.image.load("images\\logo.png")
+    new_game_paper = pygame.image.load("images\\new_game_paper_resized.png")
+    button_paper_image = pygame.image.load("images\\button_paper.png")
+    player_slot_image = pygame.image.load("images\\player_slot.png")
+    player_images = [pygame.image.load("images\\player1.png"), pygame.image.load("images\\player2.png"),
+                     pygame.image.load("images\\player3.png"), pygame.image.load("images\\player4.png"),
+                     pygame.image.load("images\\player5.png"), pygame.image.load("images\\player6.png")]
+    font1 = "fonts\\font1.ttf"
+
+    def __init__(self, screen):
+        self.screen = screen
+        self.opt_menu = False
+        self.setting_menu = False
+        self.state = 0
+        self.players = 0
+        self.AI_agents = 0
+        self.background_image = pygame.transform.scale(self.background_image,
+                                                       (screen.get_width(), screen.get_height()))
+        self.logo_image = pygame.transform.scale(self.logo_image, (int(screen.get_width() * 0.3),
+                                                                   int(screen.get_height() * 0.12)))
+        self.new_game_paper = pygame.transform.scale(self.new_game_paper, (int(screen.get_width() * 0.4),
+                                                                           int(screen.get_height() * 0.9)))
+        self.center_x, self.center_y = screen.get_width() / 2, screen.get_height() / 2
+        self.main_menu_buttons = self.create_main_menu_buttons()
+        self.new_menu_buttons = self.create_new_game_menu_buttons()
+        self.player_slots = self.create_player_slots()
+
+    def draw(self):
+        self.screen.blit(self.background_image, (0, 0))
+        self.screen.blit(self.logo_image, (int(self.center_x * 1.3), int(self.center_y * 0.1)))
+        for button in self.main_menu_buttons:
+            button.draw(self.screen)
+        self.draw_opt_menu()
+
+    def draw_opt_menu(self):
+        if self.opt_menu:
+            self.screen.blit(self.new_game_paper, (int(self.center_x * 0.08), int(self.center_y * 0.12)))
+            self.draw_text(self.screen, "Players", int(self.center_y * 0.17), (0, 0, 0), int(self.center_x * 0.14),
+                           int(self.center_y * 0.18), font=self.font1)
+            for button in self.new_menu_buttons:
+                button.draw(self.screen)
+            for slot in self.player_slots:
+                slot.draw(self.screen)
+
+    def check_clicks(self, event):
+        for button in self.main_menu_buttons:
+            button.check_click(event)
+        for button in self.new_menu_buttons:
+            button.check_click(event)
+
+    def draw_text(self, screen, text, size, color, x, y, font=None, ):
+        font = pygame.font.Font(font, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.topleft = (x, y)
+        screen.blit(text_surface, text_rect)
+
+    def set_opt_menu(self, state):
+        self.opt_menu = state
+        self.players = 0
+        self.AI_agents = 0
+
+    def set_setting_menu(self, state):
+        self.setting_menu = state
+
+    def change_state(self, new_state):
+        self.state = new_state
+
+    def get_state(self):
+        return self.state
+
+    def add_player(self):
+        if self.players < 6:
+            self.players += 1
+            self.player_slots[self.players - 1].change_image(self.player_images[self.players - 1])
+
+    def remove_player(self):
+        if self.players > 0:
+            self.player_slots[self.players - 1].change_image(self.player_slot_image)
+            self.players -= 1
+
+    def create_main_menu_buttons(self):
+        play = Button(self.button_image, (int(self.center_x * 0.2), int(self.center_y * 0.5)), "Play",
+                      int(self.center_y * 0.12), int(self.center_x * 0.35), int(self.center_y * 0.2), font=self.font1,
+                      action=lambda: self.set_opt_menu(True))
+        settings = Button(self.button_image, (int(self.center_x * 0.2), int(self.center_y * 0.8)), "Settings",
+                          int(self.center_y * 0.12), int(self.center_x * 0.35), int(self.center_y * 0.2),
+                          font=self.font1,
+                          action=lambda: self.set_setting_menu(True))
+        quit_b = Button(self.button_image, (int(self.center_x * 0.2), int(self.center_y * 1.1)), "Quit",
+                        int(self.center_y * 0.12), int(self.center_x * 0.35), int(self.center_y * 0.2), font=self.font1,
+                        action=lambda: self.change_state(-1))
+        return [play, settings, quit_b]
+
+    def create_new_game_menu_buttons(self):
+        play = Button(self.button_image, (int(self.center_x * 0.63), int(self.center_y * 1.7)), "Play",
+                      int(self.center_y * 0.08), int(self.center_x * 0.2), int(self.center_y * 0.15), font=self.font1,
+                      action=lambda: self.change_state(1))
+        back = Button(self.button_image, (int(self.center_x * 0.13), int(self.center_y * 1.7)), "Back",
+                      int(self.center_y * 0.08), int(self.center_x * 0.2), int(self.center_y * 0.15), font=self.font1,
+                      action=lambda: self.set_opt_menu(False))
+        add = Button(self.button_paper_image, (int(self.center_x * 0.45), int(self.center_y * 0.23)), "Add",
+                     int(self.center_y * 0.06), int(self.center_x * 0.17), int(self.center_y * 0.12), font=self.font1,
+                     action=lambda: self.add_player())
+        remove = Button(self.button_paper_image, (int(self.center_x * 0.65), int(self.center_y * 0.23)), "Remove",
+                        int(self.center_y * 0.06), int(self.center_x * 0.17), int(self.center_y * 0.12),
+                        font=self.font1,
+                        action=lambda: self.remove_player())
+        return [play, back, add, remove]
+
+    def create_player_slots(self):
+        slots1 = [Button(self.player_slot_image, (int(self.center_x * 0.2), int(self.center_y * i)), "",
+                         1, int(self.center_x * 0.12), int(self.center_x * 0.12))
+                  for i in np.arange(0.6, 1.5, 0.3)]
+        slots2 = [Button(self.player_slot_image, (int(self.center_x * 0.53), int(self.center_y * i)), "",
+                         1, int(self.center_x * 0.12), int(self.center_x * 0.12))
+                  for i in np.arange(0.6, 1.5, 0.3)]
+        return slots1 + slots2

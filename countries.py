d0df2b2ba217f482bb0d5baaae8f35ea1d4513d4
MisterPasha <pavel.jermolajev99@gmail.com>
2024-02-20 17:51:54 +0000

    did some code

--- countries.py
+++ countries.py
@@ -4,9 +4,12 @@ pygame.init()
 
 
 class Country:
-    def __init__(self):
+    def __init__(self, screen, image):
         self.owner = None
         self.troops = 0
+        self.screen = screen
+        self.image = pygame.transform.scale(image, (screen.get_width(), screen.get_height()))
+        self.color = (192, 192, 192)
 
     def set_owner(self, new_owner):
         self.owner = new_owner
@@ -16,6 +19,22 @@ class Country:
 
     def remove_troops(self, num_troops):
         self.troops -= num_troops
+
+    def draw(self):
+        color = pygame.Color(self.color)
+        color_image = self.fill_with_color(self.image, color)
+        self.screen.blit(color_image, (0, 0))
+
+    def fill_with_color(self, image, color):
+        coloured_image = pygame.Surface(self.image.get_size())
+        coloured_image.fill(color)
+
+        final_image = image.copy()
+        final_image.blit(coloured_image, (0, 0), special_flags=pygame.BLEND_MULT)
+        return final_image
+
+    def change_color(self, new_color):
+        self.color = new_color
 
 
 class Continent:
--- map.py
+++ map.py
@@ -1,5 +1,7 @@
 import pygame
 from button import Button
+from countries import Country
+import random
 
 pygame.init()
 
@@ -18,11 +20,14 @@ class Map:
         self.map_img = pygame.transform.scale(map_img, (screen.get_width(), screen.get_height()))
         self.state = None
         self.buttons = self.create_buttons()
+        self.countries = self.create_countries()
 
     def draw(self):
         self.screen.blit(self.map_img, (0, 0))
         for button in self.buttons:
             button.draw(self.screen)
+        for country in self.countries:
+            country.draw()
 
     def get_state(self):
         return self.state
@@ -34,11 +39,24 @@ class Map:
         for button in self.buttons:
             button.check_click(event)
 
+    def change_color(self):
+        colors = ["red", "blue", "green", "yellow", "pink", "white"]
+        for country in self.countries:
+            country.change_color(random.choice(colors))
+
     def create_buttons(self):
         buttons = []
         back = Button(button_image, button_hover_image, (0, 0), "Back",
                       int(self.center_y * 0.08), int(self.center_x * 0.1), int(self.center_y * 0.1), font=font1,
                       action=lambda: self.set_state(0))
+        change_color = Button(button_image, button_hover_image, (self.center_x,self.center_y ), "Change",
+                      int(self.center_y * 0.1), int(self.center_x * 0.2), int(self.center_y * 0.2), font=font1,
+                      action=lambda: self.change_color())
         buttons.append(back)
+        buttons.append(change_color)
         return buttons
-    
+
+    def create_countries(self):
+        country_image = pygame.image.load("images\\country1.png").convert_alpha()
+        country1 = Country(self.screen, country_image)
+        return [country1]
